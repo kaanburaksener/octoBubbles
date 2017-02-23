ee0dc821709ddc0671637db298011e3fec4dc6fb
kaanburaksener <kaanburaksener@gmail.com>
2017-02-23 05:48:46 +0300

    Test about file recognition has been performed.

--- src/com/kaanburaksener/Main.java
+++ src/com/kaanburaksener/Main.java
@@ -1,8 +1,11 @@
 package com.kaanburaksener;
 
+import com.kaanburaksener.ast.controller.NodeController;
+
 public class Main {
-
     public static void main(String[] args) {
-
+        String path = "test-source-code";
+        NodeController nodeController = new NodeController(path);
+        nodeController.initialize();
     }
 }
